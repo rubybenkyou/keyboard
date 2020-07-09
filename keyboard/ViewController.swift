0f179adb6e1cbdf14cf86d16b96adc084f08ea56
rubybenkyou <eurkurakura@yahoo.co.jp>
2020-07-09 09:39:45 +0900

    Initial Commit

---
+++ keyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  keyboard
+//
+//  Created by a on 2020/07/09.
+//  Copyright © 2020 a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
