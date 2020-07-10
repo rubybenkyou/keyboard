a39eb3a4483d8df9cec6a169abe4cf27f468341d
rubybenkyou <eurkurakura@yahoo.co.jp>
2020-07-10 10:15:04 +0900

    key

--- keyboard/ViewController.swift
+++ keyboard/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    @IBOutlet weak var userNameTextFirld: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        userNameTextFirld.delegate = self
+        passwordTextField.delegate = self
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    @IBAction func loginAction(_ sender: Any) {
+        
+        logoImageView.image = UIImage(named: "loginOK")
+        
+        userNameLabel.text = userNameTextFirld.text
+        passwordLabel.text = passwordTextField.text
+    }
+    
+    //タッチで閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+    //reterunで閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
